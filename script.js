2092bc07313a045960e845593346add7ef02c043
Mitchel Bower <mitchelbower@outlook.com>
2020-01-29 06:08:21 -0800

    revision

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// let name = "Mitch";
-// let age = 25;
-// let birthday = "november 4th";
-// let detroitGC = "true";
-// let lifeEvents = [
-//   "I was born in royal oak.",
-//   "I went to warren mott high.",
-//   "I atteneded macomb community college.",
-//   "I evaded the police on a motorcycle in summer 2019."
-// ];
+let name = "Mitch";
+let age = 25;
+let birthday = "november 4th";
+let detroitGC = "true";
+let lifeEvents = [
+  "I was born in royal oak.",
+  "I went to warren mott high.",
+  "I atteneded macomb community college.",
+  "I evaded the police on a motorcycle in summer 2019."
+];
 
-// if (detroitGC === "true") {
-//   console.log(
-//     `My name is ${name} and i am a student at Grand Circus in Detroit, i am currently ${age}, and my birthday is ${birthday}.`
-//   );
-// } else {
-//   console.log(
-//     `My name is ${name} and i am a student at Grand Circus in Grand Rapids, i am currently ${age}, and my birthday is ${birthday}.`
-//   );
+if (detroitGC === "true") {
+  console.log(
+    `My name is ${name} and i am a student at Grand Circus in Detroit, i am currently ${age}, and my birthday is ${birthday}.`
+  );
+} else {
+  console.log(
+    `My name is ${name} and i am a student at Grand Circus in Grand Rapids, i am currently ${age}, and my birthday is ${birthday}.`
+  );
+}
+
+// for (let i = 0; i < lifeEvents.length; i++) {
+//   console.log(lifeEvents[i]);
 // }
 
-// // for (let i = 0; i < lifeEvents.length; i++) {
-// //   console.log(lifeEvents[i]);
-// // }
-
-// lifeEvents.forEach(lifEvent => {
-//   console.log(lifEvent);
-// });
+lifeEvents.forEach(lifEvent => {
+  console.log(lifEvent);
+});
 
 let counter = 0;
 
